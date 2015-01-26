690b82e1c7ea79ae917617e9ef00b2df3b943156
leonardo.moura <leonardo.moura@LMOURA.pti.com.br>
2015-01-26 11:42:51 -0200

    atualizar nome das variaveis

--- BundleIIQXML/src/br/com/pbti/dto/Owner.java
+++ BundleIIQXML/src/br/com/pbti/dto/Owner.java
@@ -5,43 +5,38 @@ import org.w3c.dom.Element;
 import br.com.pbti.xml.MontarXml;
 
 public class Owner {
-	
+
 	//
 	private Element owner;
 	private Element montarOwner;
 	private Element refence;
 	//
-	private String nomeOwner;
-
+	private String ownerName;
 
 	public MontarXml montarXml = new MontarXml();
 
 	@SuppressWarnings("static-access")
-	public void montarOwner()
-	{
+	public void montarOwner() {
 		montarOwner = montarXml.getDoc().createElement("Owner");
 		setOwner(montarOwner);
 		refence();
 	}
 
 	@SuppressWarnings("static-access")
-	private void refence()
-	{
+	private void refence() {
 		refence = montarXml.getDoc().createElement("Reference");
 		refence.setAttribute("class", "sailpoint.object.Identity");
-		refence.setAttribute("name", getNomeOwner());
+		refence.setAttribute("name", getOwnerName());
 		owner.appendChild(refence);
-		
+
 	}
 
-	
-	
-	public String getNomeOwner() {
-		return nomeOwner;
+	public String getOwnerName() {
+		return ownerName;
 	}
 
-	public void setNomeOwner(String nomeOwner) {
-		this.nomeOwner = nomeOwner;
+	public void setOwnerName(String ownerName) {
+		this.ownerName = ownerName;
 	}
 
 	public Element getOwner() {
